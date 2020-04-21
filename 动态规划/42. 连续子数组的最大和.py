9ec83fb97d57b7945578b3660aa7cc7d71b385de
CLannadZSY <zsymidi@gmail.com>
2020-04-21 22:25:52 +0800

    42. 连续子数组的最大和, 添加更快的写法

--- 动态规划/42. 连续子数组的最大和.py
+++ 动态规划/42. 连续子数组的最大和.py
@@ -25,12 +25,19 @@ from typing import List
 class Solution:
 
     def maxSubArray(self, nums: List[int]) -> int:
-        if not nums:
-            return -2 ** 31
+        # if not nums:
+        #     return -2 ** 31
+        #
+        # for i in range(1, len(nums)):
+        #     nums[i] = nums[i] + max(nums[i - 1], 0)
+        # return max(nums)
 
-        for i in range(1, len(nums)):
-            nums[i] = nums[i] + max(nums[i - 1], 0)
-        return max(nums)
+        # 更快的写法
+        dp = nums
+        for idx in range(1, len(dp)):
+            if dp[idx - 1] > 0:
+                dp[idx] = dp[idx - 1] + nums[idx]
+        return max(dp)
 
 
 if __name__ == '__main__':
