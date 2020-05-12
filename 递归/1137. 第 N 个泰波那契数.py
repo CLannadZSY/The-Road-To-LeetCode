53cc577ad8a9188f523723781a4a115c0dc4eaa0
CLannadZSY <zsymidi@gmail.com>
2020-05-12 15:12:27 +0800

    1137. 第 N 个泰波那契数

---
+++ 递归/1137. 第 N 个泰波那契数.py
@@ -0,0 +1,43 @@
+"""
+1137. 第 N 个泰波那契数
+
+泰波那契序列 Tn 定义如下： 
+
+T0 = 0, T1 = 1, T2 = 1, 且在 n >= 0 的条件下 Tn+3 = Tn + Tn+1 + Tn+2
+
+给你整数 n，请返回第 n 个泰波那契数 Tn 的值。
+
+ 
+
+示例 1：
+
+输入：n = 4
+输出：4
+解释：
+T_3 = 0 + 1 + 1 = 2
+T_4 = 1 + 1 + 2 = 4
+示例 2：
+
+输入：n = 25
+输出：1389537
+ 
+
+提示：
+
+0 <= n <= 37
+答案保证是一个 32 位整数，即 answer <= 2^31 - 1。
+
+链接：https://leetcode-cn.com/problems/n-th-tribonacci-number
+"""
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        a, b, c = 0, 1, 1
+        for i in range(n) :
+            a, b, c = b, c, a+b+c
+        return a
+
+
+s = Solution()
+n = 10
+print(s.tribonacci(n))
