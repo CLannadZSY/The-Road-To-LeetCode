c1d3a3631448aeceebe0e3398065b6081fea3b62
CLannadZSY <zsymidi@gmail.com>
2020-06-06 21:32:23 +0800

    128. 最长连续序列

---
+++ 128. 最长连续序列.go
@@ -0,0 +1,46 @@
+/*
+128. 最长连续序列
+
+给定一个未排序的整数数组，找出最长连续序列的长度。
+
+要求算法的时间复杂度为 O(n)。
+
+示例:
+
+输入: [100, 4, 200, 1, 3, 2]
+输出: 4
+解释: 最长连续序列是 [1, 2, 3, 4]。它的长度为 4。
+
+链接：https://leetcode-cn.com/problems/longest-consecutive-sequence
+*/
+package main
+
+import "fmt"
+
+func longestConsecutive(nums []int) int {
+	numSet := map[int]bool{}
+	for _, num := range nums {
+		numSet[num] = true
+	}
+	longestStreak := 0
+	for num := range numSet {
+		if !numSet[num-1] {
+			currentNum := num
+			currentStreak := 1
+			for numSet[currentNum+1] {
+				currentNum++
+				currentStreak++
+			}
+			if longestStreak < currentStreak {
+				longestStreak = currentStreak
+			}
+		}
+	}
+	return longestStreak
+}
+
+func main() {
+	nums := []int{100, 4, 200, 1, 3, 2}
+	ret := longestConsecutive(nums)
+	fmt.Println(ret)
+}
