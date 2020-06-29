6aa811997d52ff2320f461289777169caeeb6047
CLannadZSY <zsymidi@gmail.com>
2020-06-29 21:44:21 +0800

    240. 搜索二维矩阵 II

---
+++ 二分查找/240. 搜索二维矩阵 II.py
@@ -0,0 +1,79 @@
+"""
+240. 搜索二维矩阵 II
+编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target。该矩阵具有以下特性：
+
+每行的元素从左到右升序排列。
+每列的元素从上到下升序排列。
+示例:
+
+现有矩阵 matrix 如下：
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+给定 target = 5，返回 true。
+
+给定 target = 20，返回 false。
+
+链接：https://leetcode-cn.com/problems/search-a-2d-matrix-ii
+"""
+
+
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        暴力渣渣
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        for row in matrix:
+            if target in row:
+                return True
+
+        return False
+
+    def searchMatrix2(self, matrix, target):
+        """
+        搜索空间的缩减
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+
+        if not matrix:
+            return False
+
+        def search_rec(left, up, right, down):
+            if left > right or up > down:
+                return False
+            elif target < matrix[up][left] or target > matrix[down][right]:
+                return False
+
+            mid = left + (right - left) // 2
+
+            row = up
+            while row <= down and matrix[row][mid] <= target:
+                if matrix[row][mid] == target:
+                    return True
+                row += 1
+
+            return search_rec(left, row, mid - 1, down) or search_rec(mid + 1, up, right, row - 1)
+
+        return search_rec(0, 0, len(matrix[0]) - 1, len(matrix) - 1)
+
+
+s = Solution()
+matrix = [
+    [1, 4, 7, 11, 15],
+    [2, 5, 8, 12, 19],
+    [3, 6, 9, 16, 22],
+    [10, 13, 14, 17, 24],
+    [18, 21, 23, 26, 30]
+]
+target = 10
+print(s.searchMatrix(matrix, target))
