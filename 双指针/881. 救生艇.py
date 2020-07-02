8cf4302f2c7d54fd368f91af2437dc131c4a9aba
CLannadZSY <zsymidi@gmail.com>
2020-07-02 17:21:32 +0800

    881. 救生艇

---
+++ 双指针/881. 救生艇.py
@@ -0,0 +1,46 @@
+"""
+881. 救生艇
+第 i 个人的体重为 people[i]，每艘船可以承载的最大重量为 limit。
+
+每艘船最多可同时载两人，但条件是这些人的重量之和最多为 limit。
+
+返回载到每一个人所需的最小船数。(保证每个人都能被船载)。
+
+ 
+
+示例 1：
+
+输入：people = [1,2], limit = 3
+输出：1
+解释：1 艘船载 (1, 2)
+示例 2：
+
+输入：people = [3,2,2,1], limit = 3
+输出：3
+解释：3 艘船分别载 (1, 2), (2) 和 (3)
+示例 3：
+
+输入：people = [3,5,3,4], limit = 5
+输出：4
+解释：4 艘船分别载 (3), (3), (4), (5)
+提示：
+
+1 <= people.length <= 50000
+1 <= people[i] <= limit <= 30000
+
+链接：https://leetcode-cn.com/problems/boats-to-save-people
+"""
+from typing import List
+
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people = sorted(people, reverse=True)
+        left, right, res = 0, len(people) - 1, 0
+        while left < right:
+            if people[left] + people[right] <= limit:
+                left, right = left + 1, right - 1
+            else:
+                left = left + 1
+            res += 1
+        return res + 1 if left == right else res
