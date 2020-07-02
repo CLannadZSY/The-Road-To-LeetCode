c813d0f1ec4cf60aa0a98744dc1877b982b351e5
CLannadZSY <zsymidi@gmail.com>
2020-07-02 18:06:02 +0800

    884. 两句话中的不常见单词

---
+++ 哈希表/884. 两句话中的不常见单词.py
@@ -0,0 +1,41 @@
+"""
+884. 两句话中的不常见单词
+给定两个句子 A 和 B 。 （句子是一串由空格分隔的单词。每个单词仅由小写字母组成。）
+
+如果一个单词在其中一个句子中只出现一次，在另一个句子中却没有出现，那么这个单词就是不常见的。
+
+返回所有不常用单词的列表。
+
+您可以按任何顺序返回列表。
+
+ 
+
+示例 1：
+
+输入：A = "this apple is sweet", B = "this apple is sour"
+输出：["sweet","sour"]
+示例 2：
+
+输入：A = "apple apple", B = "banana"
+输出：["banana"]
+ 
+
+提示：
+
+0 <= A.length <= 200
+0 <= B.length <= 200
+A 和 B 都只包含空格和小写字母
+
+链接：https://leetcode-cn.com/problems/uncommon-words-from-two-sentences
+"""
+from typing import List
+
+
+class Solution:
+    def uncommonFromSentences(self, A: str, B: str) -> List[str]:
+        aa = " ".join([A, B]).split()
+        cc = dict()
+        for a in aa:
+            cc[a] = cc.get(a, 0) + 1
+
+        return map(lambda x: x[0], filter(lambda x:x[1]==1, cc.items()))
