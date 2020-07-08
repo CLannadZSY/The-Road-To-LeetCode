cbea05069b55c2003d7994d24d342fb9f86b3cc4
CLannadZSY <zsymidi@gmail.com>
2020-07-08 21:17:04 +0800

    1480. 一维数组的动态和

---
+++ 数组/1480. 一维数组的动态和.go
@@ -0,0 +1,41 @@
+/*
+1480. 一维数组的动态和
+
+给你一个数组 nums 。数组「动态和」的计算公式为：runningSum[i] = sum(nums[0]…nums[i]) 。
+
+请返回 nums 的动态和。
+
+
+
+示例 1：
+
+输入：nums = [1,2,3,4]
+输出：[1,3,6,10]
+解释：动态和计算过程为 [1, 1+2, 1+2+3, 1+2+3+4] 。
+示例 2：
+
+输入：nums = [1,1,1,1,1]
+输出：[1,2,3,4,5]
+解释：动态和计算过程为 [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1] 。
+示例 3：
+
+输入：nums = [3,1,2,10,1]
+输出：[3,4,6,16,17]
+
+链接：https://leetcode-cn.com/problems/running-sum-of-1d-array
+*/
+package main
+
+import "fmt"
+
+func runningSum(nums []int) []int {
+	for i := 1; i < len(nums); i++ {
+		nums[i] += nums[i-1]
+	}
+	return nums
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4}
+	fmt.Println(runningSum(nums))
+}
