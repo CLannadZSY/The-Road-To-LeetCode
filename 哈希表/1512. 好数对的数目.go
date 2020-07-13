0e0743de4cca6bd37a017dc0494fc7828367da91
CLannadZSY <zsymidi@gmail.com>
2020-07-13 17:57:16 +0800

    1512. 好数对的数目

---
+++ 哈希表/1512. 好数对的数目.go
@@ -0,0 +1,50 @@
+/*
+1512. 好数对的数目
+
+给你一个整数数组 nums 。
+
+如果一组数字 (i,j) 满足 nums[i] == nums[j] 且 i < j ，就可以认为这是一组 好数对 。
+
+返回好数对的数目。
+
+
+
+示例 1：
+
+输入：nums = [1,2,3,1,1,3]
+输出：4
+解释：有 4 组好数对，分别是 (0,3), (0,4), (3,4), (2,5) ，下标从 0 开始
+示例 2：
+
+输入：nums = [1,1,1,1]
+输出：6
+解释：数组中的每组数字都是好数对
+示例 3：
+
+输入：nums = [1,2,3]
+输出：0
+
+
+提示：
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+
+链接: https://leetcode-cn.com/problems/number-of-good-pairs/
+*/
+
+func numIdenticalPairs(nums []int) int {
+    m := map[int]int{}
+    ans := 0
+    for _, num := range nums {
+        m[num]++
+    }
+
+
+    for _, y := range m{
+        if y > 1 {
+            ans += (y * (y - 1)) / 2
+        }
+    }
+    return ans
+}
