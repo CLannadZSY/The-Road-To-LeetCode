1f1daeb9547800e2162bca5cefd3d67dab310ecc
CLannadZSY <zsymidi@gmail.com>
2020-09-27 14:31:12 +0800

    40. 组合总和 II, 删除网页空格编码

--- 回溯算法/40. 组合总和 II.py
+++ 回溯算法/40. 组合总和 II.py
@@ -1,17 +1,17 @@
 """
 40. 组合总和 II
 
-给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+给定一个数组candidates和一个目标数target，找出candidates中所有可以使数字和为target的组合。
 
-candidates 中的每个数字在每个组合中只能使用一次。
+candidates中的每个数字在每个组合中只能使用一次。
 
 说明：
 
 所有数字（包括目标数）都是正整数。
-解集不能包含重复的组合。 
-示例 1:
+解集不能包含重复的组合。
+示例1:
 
-输入: candidates = [10,1,2,7,6,1,5], target = 8,
+输入: candidates =[10,1,2,7,6,1,5], target =8,
 所求解集为:
 [
   [1, 7],
@@ -19,13 +19,13 @@ candidates 中的每个数字在每个组合中只能使用一次。
   [2, 6],
   [1, 1, 6]
 ]
-示例 2:
+示例2:
 
-输入: candidates = [2,5,2,1,2], target = 5,
+输入: candidates =[2,5,2,1,2], target =5,
 所求解集为:
 [
-  [1,2,2],
-  [5]
+ [1,2,2],
+ [5]
 ]
 
 链接：https://leetcode-cn.com/problems/combination-sum-ii
