4d5fa1db2942d57385f1ffd14c1f4e44525aa420
CLannadZSY <zsymidi@gmail.com>
2020-09-29 11:27:08 +0800

    1. 等式方程的可满足性, 迁移至并查集 2. 删除网页空格编码

--- 并查集/990. 等式方程的可满足性.py
+++ 并查集/990. 等式方程的可满足性.py
@@ -1,10 +1,10 @@
 """
 990. 等式方程的可满足性
-给定一个由表示变量之间关系的字符串方程组成的数组，每个字符串方程 equations[i] 的长度为 4，并采用两种不同的形式之一："a==b" 或 "a!=b"。在这里，a 和 b 是小写字母（不一定不同），表示单字母变量名。
+给定一个由表示变量之间关系的字符串方程组成的数组，每个字符串方程 equations[i] 的长度为 4，并采用两种不同的形式之一："a==b" 或"a!=b"。在这里，a 和 b 是小写字母（不一定不同），表示单字母变量名。
 
-只有当可以将整数分配给变量名，以便满足所有给定的方程时才返回 true，否则返回 false。 
+只有当可以将整数分配给变量名，以便满足所有给定的方程时才返回true，否则返回 false。
 
- 
+
 
 示例 1：
 
@@ -28,15 +28,15 @@
 
 输入：["c==c","b==d","x!=z"]
 输出：true
- 
+
 
 提示：
 
 1 <= equations.length <= 500
 equations[i].length == 4
-equations[i][0] 和 equations[i][3] 是小写字母
-equations[i][1] 要么是 '='，要么是 '!'
-equations[i][2] 是 '='
+equations[i][0] 和equations[i][3]是小写字母
+equations[i][1] 要么是'='，要么是'!'
+equations[i][2]是'='
 
 链接：https://leetcode-cn.com/problems/satisfiability-of-equality-equations
 """
