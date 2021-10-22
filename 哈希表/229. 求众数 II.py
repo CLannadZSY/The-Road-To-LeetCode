ed8f7e612254de1254f630f7c4324e68a9ccdbda
CLannadZSY <zsymidi@gmail.com>
2021-10-22 02:55:19 +0000

    229. 求众数 II

---
+++ 哈希表/229. 求众数 II.py
@@ -0,0 +1,41 @@
+"""
+229. 求众数 II
+
+给定一个大小为 n 的整数数组，找出其中所有出现超过 ⌊ n/3 ⌋ 次的元素。
+
+示例 1：
+
+输入：[3,2,3]
+输出：[3]
+示例 2：
+
+输入：nums = [1]
+输出：[1]
+示例 3：
+
+输入：[1,1,1,3,3,2,2,2]
+输出：[1,2]
+ 
+
+提示：
+
+1 <= nums.length <= 5 * 104
+-109 <= nums[i] <= 109
+ 
+
+进阶：尝试设计时间复杂度为 O(n)、空间复杂度为 O(1)的算法解决此问题。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/majority-element-ii/
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        n_count = Counter(nums)
+        n_3 = len(nums) // 3 + 1
+        res = []
+        for x, y in n_count.items():
+            if y >= n_3:
+                res.append(x)
+        return res
