24d3c3711ecc67724d04c8e222d1850d7b56abb4
CLannadZSY <zsymidi@gmail.com>
2021-10-30 01:23:42 +0000

    260. 只出现一次的数字 III

---
+++ 位运算/260. 只出现一次的数字 III.py
@@ -0,0 +1,50 @@
+"""
+260. 只出现一次的数字 III
+
+给定一个整数数组nums，其中恰好有两个元素只出现一次，其余所有元素均出现两次。 找出只出现一次的那两个元素。你可以按 任意顺序 返回答案。
+
+
+
+进阶：你的算法应该具有线性时间复杂度。你能否仅使用常数空间复杂度来实现？
+
+
+
+示例 1：
+
+输入：nums = [1,2,1,3,2,5]
+输出：[3,5]
+解释：[5, 3] 也是有效的答案。
+示例 2：
+
+输入：nums = [-1,0]
+输出：[-1,0]
+示例 3：
+
+输入：nums = [0,1]
+输出：[1,0]
+提示：
+
+2 <= nums.length <= 3 * 104
+-231 <= nums[i] <= 231 - 1
+除两个只出现一次的整数外，nums 中的其他数字都出现两次
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/single-number-iii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+
+        d = dict()
+        res = []
+
+        for v in nums:
+            d[v] = d.get(v, 0) + 1
+
+        for x, y in d.items():
+            if y == 1:
+                res.append(x)
+            if len(res) == 2:
+                return res
+        return res
